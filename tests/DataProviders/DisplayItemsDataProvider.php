5597b74166b87cef9b4eefb1d16a63298cda0022
Dariusz <37488679+Volmarg@users.noreply.github.com>
2018-11-15 17:12:52 +0100

    1.4 adding tests for methods responsible for fetching and converting data via api

---
+++ tests/DataProviders/DisplayItemsDataProvider.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Volmarg Reiso
+ * Date: 14.11.2018
+ * Time: 19:53
+ */
+
+namespace Tests\DataProviders;
+
+
+class DisplayItemsDataProvider {
+
+}
---
+++ tests/Unit/DisplayItemsTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Volmarg Reiso
+ * Date: 14.11.2018
+ * Time: 19:19
+ */
+
+namespace Tests\Unit;
+
+
+class DisplayItemsTest {
+
+}
